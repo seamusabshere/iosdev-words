4d21272455ce6786c4097fc177a0ae8e13fccd8b
Seamus Abshere <seamus@abshere.net>
2013-03-21 15:06:41 -0500

    Initial Commit

---
+++ Proper Names/SRAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SRAAppDelegate.h
+//  Proper Names
+//
+//  Created by Seamus Abshere on 3/21/13.
+//  Copyright (c) 2013 Seamus Abshere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Proper Names/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Proper Names
+//
+//  Created by Seamus Abshere on 3/21/13.
+//  Copyright (c) 2013 Seamus Abshere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SRAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([SRAAppDelegate class]));
+  }
+}
